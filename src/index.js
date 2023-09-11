e72a42103ecb6d988bd587e95d801fab518aeb53
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-11 18:53:07 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const PORT = 3000;
 
+// andlebars config
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+// Routes
 app.get("/", (req, res) => {
-    res.send('Hello from express');
+    res.render('index');
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
