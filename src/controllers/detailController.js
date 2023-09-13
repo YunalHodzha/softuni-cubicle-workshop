b00accb3b60eaa2b8b631e95834c5e4446683ca0
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-13 18:09:14 +0300

    Add 404 Not found page

--- src/controllers/detailController.js
+++ src/controllers/detailController.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const cubeManager = require('../managers/cubeManager');
+const notFoundController = require('../controllers/notFoundController');
 
 router.get('/:id', (req, res) => {
     const id = req.params.id;
     const cube = cubeManager.getCubeById(id);
-
-    res.render('details', cube);
+    console.log(cube);
+    if (cube === undefined) {
+        res.render('404');
+    } else {
+        res.render('details', cube);
+    };
 })
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const handlebarsConfig = require('./config/handlebarsConfig.js');
 const homeController = require('./controllers/homeController.js');
 const cubeController = require('./controllers/cubeController.js');
 const detailController = require('./controllers/detailController.js');
+const notFoundController = require('./controllers/notFoundController.js');
 
 const PORT = 3000;
 
@@ -18,5 +19,7 @@ handlebarsConfig(app);
 app.use(homeController);
 app.use('/cubes', cubeController);
 app.use('/details', detailController);
+
+app.use('*', notFoundController);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
