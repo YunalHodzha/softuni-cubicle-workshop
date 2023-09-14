c809f29309e8f0af799a9570e35d91b0121922e4
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-14 18:59:02 +0300

    Add search

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,13 +1,29 @@
 const uniqId = require('uniqid');
 const cubes = [{
-    id: '1x2ulshjslmj7bhok', 
+    id: '1x2ulshjslmj7bhok',
     name: 'Pyraminx',
     description: 'So Much Fun',
     imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61izOzq%2BBAL._SY355_.jpg',
-    difficultyLevel: "1",
+    difficultyLevel: 1,
 }];
 
-exports.getAll = () => cubes.slice();
+exports.getAll = (search, from, to) => {
+    let result = cubes.slice();
+
+    if (search) {
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (from) {
+        result = result.filter(cube => cube.difficultyLevel >= Number(from));
+    }
+
+    if (to) {
+        result = result.filter(cube => cube.difficultyLevel <= Number(to));
+    }
+
+    return result;
+};
 
 exports.create = (cubeData) => {
 
