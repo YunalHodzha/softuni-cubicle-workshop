3ea35b9a5f8d16f9ee134864fb891a9776d4e684
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-14 19:21:47 +0300

    extract routes

--- src/index.js
+++ src/index.js
@@ -3,22 +3,14 @@ const mongoose = require('mongoose');
 
 const expressConfig = require('./config/expressConfig.js');
 const handlebarsConfig = require('./config/handlebarsConfig.js');
-const homeController = require('./controllers/homeController.js');
-const cubeController = require('./controllers/cubeController.js');
-
+const routes = require('./routes.js')
 const PORT = 3000;
 
 const app = express();
 
 expressConfig(app);
 handlebarsConfig(app);
+app.use(routes);
 
-
-// Routes
-app.use(homeController);
-app.use('/cubes', cubeController);
-app.get("*", (req, res) => {
-    res.redirect('/404');
-})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
