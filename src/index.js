6c22d5eec56171944bd0e987c38d82e6fd30cecd
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-16 14:34:23 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig.js');
 const handlebarsConfig = require('./config/handlebarsConfig.js');
-const dbConnect = require('./config/dbConfig.js');
-const routes = require('./routes.js')
+
+const routes = require('./routes.js');
+
 const PORT = 3000;
 
 const app = express();
 
-dbConnect()
-    .then(() => console.log('DB Connected successfully'))
-    .catch(err => console.log('DB error: ', err));
-
 expressConfig(app);
 handlebarsConfig(app);
-
 app.use(routes);
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
