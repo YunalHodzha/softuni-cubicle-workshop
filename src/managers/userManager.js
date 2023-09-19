24a0d6b90a18599e8c5a18a9c60b860bd1134e8c
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-19 16:54:58 +0300

    User login

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,5 +1,24 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
 
+exports.login = async (username, password) => {
+    // TODO find user
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Cannot find username or password');
+    }
+
+    // TODO validate password
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error('Cannot find username or password');
+    }
+
+    //return user
+    return user;
+}
 
 exports.register = (userData) => User.create(userData);
