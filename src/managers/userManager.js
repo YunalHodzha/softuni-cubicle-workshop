fd4e14686bda3ebc3c6025ce3d28b9e82cb65dc8
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-19 17:27:17 +0300

    add jwt on login

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,7 +1,10 @@
 const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
 
 const User = require('../models/User');
 
+// Terminal Code Generator: node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
+const SECRET =  'aca52a7bdd63a620527ddf03eecbf73877a418fa6e33b56e19f267b29b10599a';
 
 exports.login = async (username, password) => {
     // TODO find user
@@ -17,8 +20,16 @@ exports.login = async (username, password) => {
         throw new Error('Cannot find username or password');
     }
 
+    // TODO create token
+    const payload = {
+        _id: user._id,
+        username: user.username
+    };
+
+    const token = await jwt.sign(payload, SECRET, {expiresIn: '2d'});
+
     //return user
-    return user;
+    return token;
 }
 
 exports.register = (userData) => User.create(userData);
