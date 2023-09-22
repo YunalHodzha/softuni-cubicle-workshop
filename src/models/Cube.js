48ba5a56445972e0a2929dfab254a7b2641c8a9d
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-22 16:18:08 +0300

    add cube owner

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -8,7 +8,11 @@ const cubeShcema = new mongoose.Schema({
     accessories: [{
         type: mongoose.Types.ObjectId,
         ref: 'Accessory'
-    }]
+    }],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }
 });
 
 const Cube = mongoose.model('Cube', cubeShcema);
